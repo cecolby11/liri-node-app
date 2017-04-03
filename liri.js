a4019db63a056f1bba1ff55fc7d76b5ac59e1fc4
cecolby11 <cecolby11@users.noreply.github.com>
2017-04-02 21:47:20 -0500

    spotify

--- liri.js
+++ liri.js
@@ -4,6 +4,7 @@
 
 var inquirer = require('inquirer');
 var Twitter = require('twitter');
+var spotify = require('spotify');
 var color = require('cli-color');
 
 //============
@@ -30,7 +31,7 @@ inquirer.prompt({
       twitterActions.fetchTweets();
       break;
     case 'spotify-this-song':
-      spotifyActions.fetchSongInfo();
+      spotifyActions.getUserSongName();
   }
 });
 
@@ -47,9 +48,10 @@ var twitterActions = {
     var client = new Twitter(this.keys);
 
     //request last 20 tweets of authenticated user 
-    client.get('statuses/user_timeline', {count: 2}, function(error, tweets, response){
+    client.get('statuses/user_timeline', function(error, tweets, response){
       if(error) {
         console.log(error);
+        return;
       } else {
         // tweets--> array of tweets 
         // show tweet text and creation time 
@@ -71,9 +73,100 @@ var twitterActions = {
 };
 
 var spotifyActions = {
-  fetchSongInfo: function() {
-    
+  //default: the sign, by ace of bass
+  'songName': 'The Sign',
+
+  getUserSongName: function() {
+    inquirer.prompt({
+      type: 'input',
+      message: 'Please enter the name of a song:',
+      name: 'songName'
+    }).then(function(userData){
+      if(userData.songName !== '') {
+        spotifyActions.songName = userData.songName;
+      }
+        spotifyActions.fetchTrackMatches();
+    });
+  },
+
+  fetchTrackMatches: function() {
+    var endpointURL = 'https://api.spotify.com/v1/search?';
+    var queryURL = endpointURL + 'q=' +spotifyActions.songName + '&type=track&limit=5'
+    spotify.get(queryURL, function(error, data) {
+      if(error){
+        console.log(error);
+      } else {
+        var resultsArr = data.tracks.items;
+        spotifyActions.whichArtist(resultsArr);
+      }
+    });
+  }, 
+
+  whichArtist: function(resultsArr) {
+    var artistChoices = [];
+    for(var i = 0; i<resultsArr.length; i++) {
+      var song = resultsArr[i];
+      var songArtists = '';
+      songArtists = song.artists[0].name;
+      if (songArtists.length > 1) {
+        for(var j=1; j<song.artists.length; j++){
+            songArtists += ' & ' + song.artists[j].name;
+        }
+      }
+      //create array of the top 5 artist options
+      artistChoices.push(songArtists);
+    } 
+
+    // ask the user which artist they want the info for
+    inquirer.prompt({
+      type: "list",
+      choices: artistChoices,
+      message: 'Please confirm which artist you\'re looking for:',
+      name: 'artist'
+    }).then(function(userData){
+      // get index of that artist in the choices presented
+      var index = artistChoices.indexOf(userData.artist);
+      // index corresponds to their index in the items array 
+      spotifyActions.displayTrackInfo(resultsArr[index]);
+      // 
+    });
+  }, 
+
+  displayTrackInfo: function(trackItem) {
+    console.log(color.magentaBright('\n================   Here is the track info you wanted!  ================\n\n'));
+
+    //song name
+    console.log(color.bgMagenta("Track Name"));
+    console.log(color.magenta(trackItem.name +'\n'));
+
+    //artists 
+    var songArtists = '';
+      songArtists = trackItem.artists[0].name;
+      if (songArtists.length > 1) {
+        for(var j=1; j<trackItem.artists.length; j++){
+            songArtists += ' & ' + trackItem.artists[j].name;
+        }
+      }
+    console.log(color.bgMagenta("Track Artist(s)"));
+    console.log(color.magenta(songArtists + '\n'));
+
+    // album
+    var album = trackItem.album.name;
+    console.log(color.bgMagenta("Album Name"));
+    console.log(color.magenta(album) + '\n');
+
+    // preview url 
+    var previewURL = trackItem.preview_url;
+    console.log(color.bgMagenta("Preview Url"));
+    console.log(color.magenta(previewURL) + '\n');
   }
+
+
 
 };
 
+
+
+//SPOTIFY todos: 
+// handle spaces in song name 
+// add 'other' option where user can type in an alternate artist which will be added to the query
